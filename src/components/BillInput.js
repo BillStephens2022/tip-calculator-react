6e6d9ea5f212877d668f454e0fae8ebce00a2f13
BillStephens2022 <two4onebill@yahoo.com>
2024-07-10 06:52:39 -0400

    adjusted styling

--- src/components/BillInput.js
+++ src/components/BillInput.js
@@ -1,6 +1,6 @@
 export function BillInput({ bill, setBill }) {
     return (
-        <form>
+        <form className="form-group">
             <label>How much is the bill?</label>
             <input type="text" placeholder="enter total bill amount" onChange={(e) => setBill(Number(e.target.value))}/>
         </form>
